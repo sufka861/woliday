5629368ade4a1d76cf03afbfddf9d2e027217764
ronina24 <roni24682@gmail.com>
2023-01-15 21:18:22 +0200

    in myRoute.html: map with display of the route, checkbox with db information according to route order of addresses

--- router/squads.js
+++ router/squads.js
@@ -6,10 +6,12 @@ const {
     createSquad,
     updateSquad,
     deleteSquad,
+    getSquadByll,
 } = require('../controller/squads');
 
 router.get('/', getAllSquads);
 router.get('/:squadId', getSquadById);
+router.get('/squad/:squadId/ll/:ll', getSquadByll);
 router.post('/', createSquad);
 router.put('/:squadId', updateSquad);
 router.delete('/:squadId', deleteSquad);
