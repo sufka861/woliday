52c7aa366e163f490294d898eb9b816ace98bdc3
Suf <sufkarmon2@gmail.com>
2023-01-26 23:04:02 +0200

    erase unwanted sneat from router

--- router/client.js
+++ router/client.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const clientController = require('../controller/client.controller');
 const client = express.Router();
-// const sneat = express.Router();
 
 client.get('*', clientController.loadPage);
-// sneat.get('*', clientController.loadPage);
 
 module.exports = client
-// {
-//     client,
-//     // sneat
-// };
+
