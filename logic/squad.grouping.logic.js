cc080cab1265ba9806641ffcc6d872084a8e2459
Suf <sufkarmon2@gmail.com>
2023-01-28 23:07:22 +0200

    load families into squads working good, not showing on route page

--- logic/squad.grouping.logic.js
+++ logic/squad.grouping.logic.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const squadsModel = require('../models/squads');
 const User = require('../models/user');
 const Family = require('../models/families');
-const Squads = require("../models/squads");
+// const Squads = require("../models/squads");
 const axios = require("axios");
 
 const updateUser = async (user_id, squad_id) => {
@@ -54,20 +54,19 @@ const groupUsersIntoSquads = async () => {
 
 
 const groupFamiliesIntoSquads = async () => {
-    const families = await Family.familyModel.find({});
+    let families = await Family.familyModel.find({});
     const numOfFamilies = families.length;
     const squads = await squadsModel.find({});
     const numOfSquads = squads.length;
     const numOfFamiliesPerSquad = numOfFamilies / numOfSquads;
     squads.forEach((squad) => {
-        let families_per_squad = [];
         for (let i = 0; i < numOfFamiliesPerSquad; i++) {
-            families_per_squad.push(families.pop())
-        }
-        squadsModel.findByIdAndUpdate({_id: squad._id}, {$set: {families: families_per_squad}})
-        console.log(squad.families);
+            const family = families.pop();
+            const result = squadsModel.updateOne({_id: squad._id}, {$push : {families: family}});
+       }
     })
 }
+// const result = groupFamiliesIntoSquads();
 
 // // JUST FOR DEBUG
 // const deleteAllSquads = () => {
